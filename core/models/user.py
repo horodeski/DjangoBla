e677058cc6d7b28b8eb08ace3f0a1563d870d5f1
horodeski <geovana.horodeski06@gmail.com>
2023-11-22 19:33:45 -0300

    Adição de mais campos para o usuario

--- core/models/user.py
+++ core/models/user.py
@@ -1,43 +1,37 @@
-"""
-Database models.
-"""
-from django.contrib.auth.models import (
-    AbstractBaseUser,
-    BaseUserManager,
-    PermissionsMixin,
-)
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.core.validators import RegexValidator
 from django.db import models
 
-
 class UserManager(BaseUserManager):
-    """Manager for users."""
 
-    def create_user(self, email, password=None, **extra_fields):
-        """Create, save and return a new user."""
+    def create_user(self, email, password=None, name=None, description=None, usuario=None, **extra_fields):
         if not email:
             raise ValueError("Users must have an email address.")
 
-        user = self.model(email=self.normalize_email(email), **extra_fields)
+        alphanumeric = RegexValidator(r'^[a-zA-Z0-9]*$', 'Only alphanumeric characters are allowed.')
+        if usuario:
+            alphanumeric(usuario)
+
+        user = self.model(email=self.normalize_email(email), name=name, description=description, usuario=usuario, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def create_superuser(self, email, password):
-        """Create, save and return a new superuser."""
-        user = self.create_user(email, password)
+    def create_superuser(self, email, password, name=None, description=None, usuario=None):
+        user = self.create_user(email, password, name=name, description=description, usuario=usuario)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
 
         return user
 
-
 class User(AbstractBaseUser, PermissionsMixin):
-    """User model in the system."""
 
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    usuario = models.CharField(max_length=255, blank=True, null=True, validators=[RegexValidator(r'^[a-zA-Z0-9]*$', 'Only alphanumeric characters are allowed.')])  # Adicionando campo de usuário com validação
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
