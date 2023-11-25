acafceaa1d5af51b69f275cac9268f708deee518
VictoorLDS <vlds221005@gmail.com>
2023-11-25 01:27:42 -0300

    feat: authentication working

--- core/views/avaliacao.py
+++ core/views/avaliacao.py
@@ -1,9 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
 
+
 from core.models import Avaliacao
 from core.serializers import AvaliacaoSerializer
+
 
 
 class AvaliacaoViewSet(ModelViewSet):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
+
